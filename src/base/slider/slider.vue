cbaa5c3ad74ead2a0e65c5e0fb21ab9bc11b4821
Yfling <598184085@qq.com>
2017-06-30 15:47:06 +0800

    滚动细节优化

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -107,6 +107,10 @@
           this.slider.goToPage(pageIndex, 0, 400);
         }, this.interval)
       }
+    },
+    destroyed() {
+      // 清理
+      clearTimeout(this.timer);
     }
   }
 </script>
