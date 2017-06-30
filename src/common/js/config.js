0dcdf877e43bf6fb56bbc63314e1069ff405b721
Yfling <598184085@qq.com>
2017-06-30 15:19:52 +0800

    初始化样式+滚动组件

---
+++ src/common/js/config.js
@@ -0,0 +1,6 @@
+// url的配置参数[因为是固定不变的]
+export const playMode = {
+  sequence: 0,
+  loop: 1,
+  random: 2
+}
---
+++ src/common/js/jsonp.js
@@ -0,0 +1,27 @@
+// 引入原始jsonp
+import originJsonp from 'jsonp'
+
+// 对外暴露一个方法
+export default function jsonp(url, data, option) {
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+
+  return new Promise((resolve, reject) => {
+    originJsonp(url, option, (err, data) => {
+      // 成功
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+export function param(data) {
+  let url = ''
+  for (var k in data) {
+    let value = data[k] !== undefined ? data[k] : ''
+    url += '&' + k + '=' + encodeURIComponent(value)
+  }
+  return url ? url.substring(1) : ''
+}
